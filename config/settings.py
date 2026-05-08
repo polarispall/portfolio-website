f3ee88985812bf7acd513c157e978c9cffa9758a
Polaris Pall <git@polarispall.com>
2026-05-08 03:03:50 -0400

    django - settings.py enabled default backend

--- config/settings.py
+++ config/settings.py
@@ -83,6 +83,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
